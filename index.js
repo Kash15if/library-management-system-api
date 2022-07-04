96d60a5a21adf5ab9169655aff4a9b14c538e35c
Kash15if <kash15if@yahoo.com>
2022-07-04 09:51:51 +0530

    added sql connection and dataconfig

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
+require("dotenv").config();
 
-const { Sequelize } = require('sequelize');
+const port = process.env.SERVER_PORT || 3000;
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
-  });
+const sequelize = require("./config/DB_Con");
 
+const doCon = async () => {
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
+};
 
-
-
-
+doCon();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
